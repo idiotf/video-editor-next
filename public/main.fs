163689b390391a67cc77abea6f568e230129ade1
IDIOT <140891554+idiotf@users.noreply.github.com>
2024-11-22 09:38:01 +0000

    modified:   public/main.fs

--- public/main.fs
+++ public/main.fs
@@ -12,11 +12,14 @@ struct Ray {
   vec3 pos;
   vec3 vec;
 };
+uniform sampler2D u_texture;
 uniform float u_alpha;
 uniform float u_fov;
 uniform vec2 u_resolution;
 uniform mat4 u_matrix;
 uniform vec3 u_vertices[triangleLength];
+uniform vec3 u_normals[triangleLength];
+uniform vec3 u_coords[triangleLength];
 
 float rand(vec2 co) {
   return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
@@ -36,7 +39,7 @@ vec3 hitTrianglePoint(vec3 v1, vec3 v2, vec3 v3, vec3 p1, vec3 p2) {
   float a = v1.y*(v2.z - v3.z) + v2.y*(v3.z - v1.z) + v3.y*(v1.z - v2.z);
   float b = v1.z*(v2.x - v3.x) + v2.z*(v3.x - v1.x) + v3.z*(v1.x - v2.x);
   float c = v1.x*(v2.y - v3.y) + v2.x*(v3.y - v1.y) + v3.x*(v1.y - v2.y);
-  float d = -(v1.x * (v2.y*v3.z - v3.y*p2.z) + v2.x * (v3.y*v1.z - v1.y*v3.z) + v3.x*(v1.y * v2.z - v2.y*v1.z));
+  float d = -(v1.x * (v2.y*v3.z - v3.y*p2.z) + v2.x * (v3.y*v1.z - v1.y*v3.z) + v3.x * (v1.y*v2.z - v2.y*v1.z));
   float dx = p2.x - p1.x;
   float dy = p2.y - p1.y;
   float dz = p2.z - p1.z;
@@ -61,6 +64,18 @@ bool isHitTriangle(vec3 v1, vec3 v2, vec3 v3, vec3 p) {
   return true;
 }
 
+bool inRange(float x, float a, float b) {
+  return a <= x && x <= b || b <= x && x <= a;
+}
+
+float map(float x, float imin, float imax, float omin, float omax) {
+  return omin + (x - imin) * (omax - omin) / (imax - imin);
+}
+
+vec3 interpolation(vec3 v1, vec3 v2, vec3 v3, vec3 n1, vec3 n2, vec3 n3, vec3 p) {
+  int l0 = inRange()
+}
+
 void main() {
   vec2 uv = gl_FragCoord.xy / u_resolution * 2.0 - 1.0;
   uv *= u_fov / 2.0;
@@ -81,8 +96,14 @@ void main() {
       vec3 v1 = u_vertices[i * 3 + 0];
       vec3 v2 = u_vertices[i * 3 + 1];
       vec3 v3 = u_vertices[i * 3 + 2];
-      if (!isHitTriangle(v1, v2, v3, hitTrianglePoint(v1, v2, v3, ray.pos, ray.vec))) break;
+      vec3 n1 = u_normals[i * 3 + 0];
+      vec3 n2 = u_normals[i * 3 + 1];
+      vec3 n3 = u_normals[i * 3 + 2];
+      vec3 p  = hitTrianglePoint(v1, v2, v3, ray.pos, ray.vec);
+      if (!isHitTriangle(v1, v2, v3, p)) break;
       TriangleInfo info = color(i);
+      vec3 normal = normalize(vec3(interpolation(v1, v2, v3, n1, n2, n3, p)));
+
     }
   }
 
